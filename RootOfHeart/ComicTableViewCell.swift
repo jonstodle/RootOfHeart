375a947ddf8a075cdf98391cca287e13a3113b9b
Unknown <jonstodle@outlook.com>
2016-11-20 20:16:54 +0100

    Add storyboard and backing classes

---
+++ RootOfHeart/ComicTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ComicTableViewCell.swift
+//  RootOfHeart
+//
+//  Created by Jon Stødle on 20/11/2016.
+//  Copyright © 2016 Jon Stødle. All rights reserved.
+//
+
+import UIKit
+
+class ComicTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
