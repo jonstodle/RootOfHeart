a3565d836707de4c9edc2d7354645e50f1b9ad45
Unknown <jonstodle@outlook.com>
2016-11-22 11:38:40 +0100

    Add comic view

---
+++ RootOfHeart/ComicViewController.swift
@@ -0,0 +1,55 @@
+//
+//  ComicViewController.swift
+//  RootOfHeart
+//
+//  Created by Jon Stødle on 22/11/2016.
+//  Copyright © 2016 Jon Stødle. All rights reserved.
+//
+
+import UIKit
+
+class ComicViewController: UIViewController {
+    
+    // MARK: - Private Properties
+    
+    private var _comic: Comic?
+
+    
+    
+    
+    // MARK: - Implementation
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    
+    // MARK: - Public Methods
+    
+    func set(comic: Comic){
+        _comic = comic
+        
+        title = _comic?.title
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
