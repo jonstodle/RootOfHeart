a958e856dcb769cbef6fcc213666c7e2ebd47b5d
Unknown <jonstodle@outlook.com>
2016-12-05 12:21:32 +0100

    Add file service

---
+++ RootOfHeart/FileService.swift
@@ -0,0 +1,53 @@
+//
+//  FileService.swift
+//  RootOfHeart
+//
+//  Created by Jon Stødle on 05/12/2016.
+//  Copyright © 2016 Jon Stødle. All rights reserved.
+//
+
+import UIKit
+import Foundation
+import RxSwift
+
+class FileService {
+    static func saveImageToCache(name: String, image: UIImage) -> Observable<Bool>{
+        return Observable<Bool>.create{
+            o in
+            var disposed = false;
+            
+            let paths = NSSearchPathForDirectoriesInDomains(.cachesDirectory, .userDomainMask, true)
+            let filePath = paths.first!.appending("/\(name)")
+            
+            if !disposed {
+                try? UIImageJPEGRepresentation(image, 1.0)?.write(to: URL(fileURLWithPath: filePath))
+                
+                o.onNext(true)
+                o.onCompleted()
+            }
+            
+            return Disposables.create {
+                disposed = true
+            }
+        }
+    }
+    
+    static func loadImageFromCache(name: String) -> Observable<UIImage> {
+        return Observable.create{
+            o in
+            var disposed = false
+            
+            let paths = NSSearchPathForDirectoriesInDomains(.cachesDirectory, .userDomainMask, true)
+            let filePath = paths.first!.appending(name)
+            
+            if !disposed {
+                o.onNext(UIImage(contentsOfFile: filePath)!)
+                o.onCompleted()
+            }
+            
+            return Disposables.create {
+                disposed = true
+            }
+        }
+    }
+}
