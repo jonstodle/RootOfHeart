3c90732061a66907dc9092f9a37b6e7962bc2c5c
Unknown <jonstodle@outlook.com>
2016-12-08 22:11:54 +0100

    Make filtering based on favorites and unread work

--- RootOfHeart/HomeTableViewController.swift
+++ RootOfHeart/HomeTableViewController.swift
@@ -27,7 +27,9 @@ class HomeTableViewController: UITableViewController {
     
     fileprivate let _disposeBag = DisposeBag()
     fileprivate let _comicCellIdentifier = "comicCell"
-    fileprivate var _notificationToken: NotificationToken?
+    fileprivate var _allNotificationToken: NotificationToken?
+    fileprivate var _favoritesNotificationToken: NotificationToken?
+    fileprivate var _unreadNotificationToken: NotificationToken?
     
     
     
@@ -39,32 +41,38 @@ class HomeTableViewController: UITableViewController {
         
         tableView.contentOffset = CGPoint(x: 0, y: headerView.frame.height)
         
-        _notificationToken = DataService.instance.comics.addNotificationBlock({ changes in
-            guard let tableView = self.tableView else{return}
-            
-            switch changes{
-            case .initial:
-                tableView.reloadData()
-                break
-            case .update(_, deletions: let deletions, insertions: let insertions, modifications: let modifcations):
-                tableView.beginUpdates()
-                tableView.insertRows(at: insertions.map{IndexPath(row: $0, section: 0)}, with: .automatic)
-                tableView.deleteRows(at: deletions.map{IndexPath(row: $0, section: 0)}, with: .automatic)
-                tableView.reloadRows(at: modifcations.map{IndexPath(row: $0, section: 0)}, with: .automatic)
-                tableView.endUpdates()
-                break
-            case .error:
-                // Handle error
-                break
+        _allNotificationToken = DataService.instance.comics.addNotificationBlock({
+            if self.headerSegmentedControl.selectedSegmentIndex == 1{
+                self.updateTableView(with: $0)
             }
         })
+        
+        _favoritesNotificationToken = DataService.instance.favoritedComics.addNotificationBlock({
+            if self.headerSegmentedControl.selectedSegmentIndex == 0{
+                self.updateTableView(with: $0)
+            }
+        })
+        
+        _unreadNotificationToken = DataService.instance.unreadComics.addNotificationBlock({
+            if self.headerSegmentedControl.selectedSegmentIndex == 2{
+                self.updateTableView(with: $0)
+            }
+        })
+        
+        headerSegmentedControl.rx
+            .value
+            .subscribe(onNext:{
+                _ in
+                self.tableView.reloadData()
+            })
+            .addDisposableTo(_disposeBag)
         
         self.tableView.rx
             .itemSelected
             .subscribe(
                 onNext:{ indexPath in
                     let newVc = self.storyboard?.instantiateViewController(withIdentifier: "ComicViewController") as! ComicViewController
-                    newVc.comic = DataService.instance.comics[indexPath.row]
+                    newVc.comic = self.getCurrentSource()[indexPath.row]
                     self.navigationController?.pushViewController(newVc, animated: true)
                 })
             .addDisposableTo(_disposeBag)
@@ -80,7 +88,38 @@ class HomeTableViewController: UITableViewController {
     // MARK: - Initializers
     
     deinit {
-        _notificationToken?.stop()
+        _allNotificationToken?.stop()
+    }
+    
+    
+    
+    // MARK: - Table View Data Source
+    
+    fileprivate func getCurrentSource() -> Results<Comic> {
+        let i = headerSegmentedControl.selectedSegmentIndex
+        
+        if i == 0 { return DataService.instance.favoritedComics }
+        if i == 2 { return DataService.instance.unreadComics }
+        
+        return DataService.instance.comics
+    }
+    
+    fileprivate func updateTableView(with realmChanges: RealmCollectionChange<Results<Comic>>) {
+        switch realmChanges{
+        case .initial:
+            tableView.reloadData()
+            break
+        case .update(_, deletions: let deletions, insertions: let insertions, modifications: let modifcations):
+            tableView.beginUpdates()
+            tableView.insertRows(at: insertions.map{IndexPath(row: $0, section: 0)}, with: .automatic)
+            tableView.deleteRows(at: deletions.map{IndexPath(row: $0, section: 0)}, with: .automatic)
+            tableView.reloadRows(at: modifcations.map{IndexPath(row: $0, section: 0)}, with: .automatic)
+            tableView.endUpdates()
+            break
+        case .error:
+            // Handle error
+            break
+        }
     }
 }
 
@@ -92,13 +131,13 @@ extension HomeTableViewController {
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return DataService.instance.comics.count
+        return getCurrentSource().count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: _comicCellIdentifier, for: indexPath) as! ComicTableViewCell
         
-        let comic = DataService.instance.comics[indexPath.row]
+        let comic = getCurrentSource()[indexPath.row]
         
         cell.comicImageView.image = nil
         cell.comicImageView.kf.setImage(with: URL(string: comic.imageUrl))
