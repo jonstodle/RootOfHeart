b0aac5938683068a3ff83d7678c4f96c6c6615f6
Unknown <jonstodle@outlook.com>
2016-12-09 12:16:20 +0100

    Add unread indicator

--- RootOfHeart/ComicTableViewCell.swift
+++ RootOfHeart/ComicTableViewCell.swift
@@ -15,6 +15,7 @@ class ComicTableViewCell: UITableViewCell {
 
     @IBOutlet weak var comicImageView: UIImageView!
     @IBOutlet weak var favoriteImageView: UIImageView!
+    @IBOutlet weak var unreadImageView: UIImageView!
     @IBOutlet weak var numberLabel: UILabel!
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var dateLabel: UILabel!
