3472433c91002462230db262a85c4bb2d7543050
Unknown <jonstodle@outlook.com>
2016-12-14 23:39:35 +0100

    Move common tasks when going to background or foreground to functions

--- RootOfHeart/AppDelegate.swift
+++ RootOfHeart/AppDelegate.swift
@@ -22,32 +22,38 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
         
-        DataService.instance.cancelAllOperations()
+        prepareForBackground()
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
         
-        DataService.instance.cancelAllOperations()
+        prepareForBackground()
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
         
-        DataService.instance.startLoadingComics()
+        prepareForForeground()
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
         
-        DataService.instance.startLoadingComics()
+        prepareForForeground()
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
-
+    func prepareForBackground(){
+        DataService.instance.cancelAllOperations()
+    }
+    
+    func prepareForForeground(){
+        DataService.instance.startLoadingComics()
+    }
 }
 
